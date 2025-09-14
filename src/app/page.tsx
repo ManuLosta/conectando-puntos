9732c26441eeab7c04eb776251c727b6cca1b1a3
ManuLosta <mlostalom@gmail.com>
2025-09-14 00:46:43 -0300

    fix: page min h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ function DashboardData() {
         </div>
       </header>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="min-h-[100vh] flex-1 rounded-xl p-4">
+        <div className="flex-1 rounded-xl p-4">
           {/* Metrics Cards */}
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 mb-6">
             <Suspense
